505653ff3995c6358c9c78ee60b9d6306ab09391
Priyam216 <spriyam2002@gmail.com>
2025-04-29 21:16:16 +0530

    redis and queue settings

---
+++ src/queue/emailQueue.js
@@ -0,0 +1,12 @@
+const { Queue } = require('bullmq');
+const IORedis = require('ioredis');
+
+const connection = new IORedis({
+    maxRetriesPerRequest: null,
+}); 
+
+const emailQueue = new Queue('emailQueue', {
+  connection,
+});
+
+module.exports = emailQueue;
